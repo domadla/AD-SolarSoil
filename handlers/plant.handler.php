b5bff115f4ec412d70b24abd508cedaf400d85e1
MarkGamboa <202310110@fit.edu.ph>
2025-07-04 20:56:42 +0800

    feat: Add plant data handler with detailed plant information

---
+++ handlers/plant.handler.php
@@ -0,0 +1,60 @@
+<?php
+$featuredPlants = [
+    [
+        "name" => "Nebula Bloom",
+        "price" => 35,
+        "desc" => "A radiant flower that glows with the colors of cosmic clouds.",
+        'img' => '/pages/shop/assets/img/plants/NebulaBloom.png',
+        "imgClass" => "nebula-bloom"
+    ],
+    [
+        "name" => "Lunar Cactus",
+        "price" => 28,
+        "desc" => "Tough and resilient, it absorbs moonlight to bloom at night.",
+        'img' => '/pages/shop/assets/img/plants/LunarCactus.png',
+        "imgClass" => "lunar-cactus"
+    ],
+    [
+        "name" => "Meteor Fern",
+        "price" => 42,
+        "desc" => "Feathers shimmer like meteors streaking through space dust.",
+        'img' => '/pages/shop/assets/img/plants/MeteorFern.png',
+        "imgClass" => "meteor-fern"
+    ],
+    [
+        "name" => "Solar Vine",
+        "price" => 30,
+        "desc" => "Golden tendrils that sway toward sunlight, storing solar energy.",
+        'img' => '/pages/shop/assets/img/plants/SolarVine.png',
+        "imgClass" => "solar-vine"
+    ],
+    [
+        "name" => "Galaxy Orchid",
+        "price" => 55,
+        "desc" => "Petals with shifting star-like specks, a true interstellar marvel.",
+        'img' => '/pages/shop/assets/img/plants/GalaxyOrchid.png',
+        "imgClass" => "galaxy-orchid"
+    ],
+    [
+        "name" => "Comet Ivy",
+        "price" => 26,
+        "desc" => "Fast-growing vine that leaves glowing trails in the dark.",
+        'img' => '/pages/shop/assets/img/plants/CometIvy.png',
+        "imgClass" => "comet-ivy"
+    ],
+    [
+        "name" => "Asteroid Moss",
+        "price" => 18,
+        "desc" => "Spongy moss that thrives in zero-gravity and rocky terrain.",
+        'img' => '/pages/shop/assets/img/plants/AsteroidMoss.png',
+        "imgClass" => "asteroid-moss"
+
+    ],
+    [
+        "name" => "Venus Bell",
+        "price" => 33,
+        "desc" => "Bell-shaped bloom that hums gently when touched.",
+        'img' => '/pages/shop/assets/img/plants/VenusBell.png',
+        "imgClass" => "venus-bell"
+    ]
+];
