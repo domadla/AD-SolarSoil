190d9cf6cc6ed76e07a9d505ecdda5ae9e599dab
MarkGamboa <202310110@fit.edu.ph>
2025-07-04 20:52:33 +0800

    feat: Define base paths conditionally in bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,10 @@
 <?php
-define('BASE_PATH', realpath(__DIR__));
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', realpath(__DIR__));
+    define('UTILS_PATH', BASE_PATH . '/utils/');
+    define('VENDOR_PATH', BASE_PATH . '/vendor/');
+    define('HANDLERS_PATH', BASE_PATH . '/handlers/');
+    define('COMPONENTS_PATH', BASE_PATH . '/components/');
+}
 
 chdir(BASE_PATH);
