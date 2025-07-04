28b2407171316299cb7a3eb33a1a62fe424660c2
MarkGamboa <202310110@fit.edu.ph>
2025-07-04 20:54:21 +0800

    feat: Add footer component with copyright notice and script inclusion

---
+++ components/templates/footer.component.php
@@ -0,0 +1,10 @@
+</main>
+
+<footer class="footer" role="contentinfo">
+    <p>© 2024 SolarSoil. All rights reserved.</p>
+</footer>
+
+<script src="/assets/js/script.js"></script>
+</body>
+
+</html>
