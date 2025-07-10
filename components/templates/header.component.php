ad0c9904bf5396cfdb25dbc0d33c117bc12c9993
MarkGamboa <202310110@fit.edu.ph>
2025-07-11 00:22:20 +0800

    feat: add Shop link to the navigation menu in header component

--- components/templates/header.component.php
+++ components/templates/header.component.php
@@ -37,6 +37,9 @@
                         <a class="nav-link" href="../Home/index.php"><i class="fas fa-home me-1"></i>Home</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link" href="../Shop/index.php"><i class="fas fa-shopping-cart me-1"></i>Shop</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="../../logout.php"><i class="fas fa-sign-out-alt me-1"></i>Logout</a>
                     </li>
                 </ul>
