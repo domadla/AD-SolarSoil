137f9b99a707b87891746f4606d4a06ee7a79a0f
MarkGamboa <202310110@fit.edu.ph>
2025-07-11 00:23:16 +0800

    refactor: update page layout to allow dynamic page variables and improve CSS/JS inclusion

--- layouts/page-layout.php
+++ layouts/page-layout.php
@@ -5,17 +5,20 @@ require_once '../../bootstrap.php';
 // Start session for user authentication
 session_start();
 
-// Page variables - CUSTOMIZE THESE FOR EACH PAGE
-$page_title = 'SolarSoil - Page Title';
-$page_description = 'Page description for SEO and meta tags.';
-$body_class = 'dashboard-page';
+// Page variables - These should be set by the calling page
+if (!isset($page_title))
+    $page_title = 'SolarSoil - Page Title';
+if (!isset($page_description))
+    $page_description = 'Page description for SEO and meta tags.';
+if (!isset($body_class))
+    $body_class = 'dashboard-page';
 
 // Include header component
 include TEMPLATES_PATH . '/header.component.php';
 ?>
 
 <!-- Page-specific CSS -->
-<link rel="stylesheet" href="assets/css/page.css">
+<link rel="stylesheet" href="assets/css/<?php echo str_replace('-page', '', $body_class); ?>.css">
 
 <!-- Animated Star Field Background -->
 <div class="stars"></div>
@@ -25,17 +28,10 @@ include TEMPLATES_PATH . '/header.component.php';
 <div class="stars5"></div>
 
 <!-- PAGE CONTENT -->
-<div class="container py-5">
-    <div class="row">
-        <div class="col-12">
-            <h1>Page Title</h1>
-            <p>Your content...</p>
-        </div>
-    </div>
-</div>
+<?php echo isset($content) ? $content : '<div class="container py-5"><div class="row"><div class="col-12"><h1>Page Title</h1><p>Your content...</p></div></div></div>'; ?>
 
 <!-- Page-specific JavaScript -->
-<script src="assets/js/page.js"></script>
+<script src="assets/js/<?php echo str_replace('-page', '', $body_class); ?>.js"></script>
 
 <?php
 // Include footer component
