9ca62bca63c17da526a4c4599b14081ec35d77b7
carlmarinyo <marinojerome2@gmail.com>
2025-07-12 00:02:29 +0800

    refactor: update syntax of user table to match postgresql

--- database/users.model.sql
+++ database/users.model.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS Users  (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Users (
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     firstname VARCHAR(150) NOT NULL,
     lastname VARCHAR(150) NOT NULL,
-    role ENUM('user', 'admin') DEFAULT 'user',
+    role TEXT DEFAULT 'user' CHECK (role IN ('user', 'admin')),
     address TEXT,
     isDeleted BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT NOW()
 );
