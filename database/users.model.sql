d94b73092a397360b6bed5ff6d98b70bf5f9c941
carlmarinyo <marinojerome2@gmail.com>
2025-07-11 13:23:00 +0800

    feat: create table schema for users

---
+++ database/users.model.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    firstname VARCHAR(150) NOT NULL,
+    lastname VARCHAR(150) NOT NULL,
+    role ENUM('user', 'admin') DEFAULT 'user',
+    address TEXT,
+    isDeleted BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
