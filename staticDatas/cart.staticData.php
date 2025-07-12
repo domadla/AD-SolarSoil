3d7d9b2fb611200c09bfb4859e1349e55913d22f
carlmarinyo <marinojerome2@gmail.com>
2025-07-13 03:14:35 +0800

    refactor: remove cart_id in static data

--- staticDatas/cart.staticData.php
+++ staticDatas/cart.staticData.php
@@ -1,15 +1,12 @@
 <?php
 return [
     [
-        'cart_id' => 1,
         'user_id' => 1,
     ],
     [
-        'cart_id' => 2,
         'user_id' => 2,
     ],
     [
-        'cart_id' => 3,
         'user_id' => 3,
     ]
 ];
