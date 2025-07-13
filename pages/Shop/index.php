431acbbc244acadd5dcdea5d9d8eec43474d490d
Marlo522 <202311932@fit.edu.ph>
2025-07-13 21:41:04 +0800

    refactor: remove inline success message handling for cart operations

--- pages/Shop/index.php
+++ pages/Shop/index.php
@@ -202,11 +202,7 @@ ob_start();
 if (isset($_GET['success'])) {
     switch ($_GET['success']) {
         case 'item_added':
-            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <i class="fas fa-check-circle me-2"></i>
-                    Item successfully added to your cart!
-                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                  </div>';
+            // Success message will be handled by JavaScript on the frontend
             break;
     }
 }
