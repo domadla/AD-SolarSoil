697b92690755641c15d4ab904945f1025b2f1397
Marlo522 <202311932@fit.edu.ph>
2025-07-13 13:29:42 +0800

    refactor: update bootstrap path in carts handler

--- handlers/carts.handler.php
+++ handlers/carts.handler.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../bootstrap.php';
+require_once BASE_PATH . '/bootstrap.php';
 require_once UTILS_PATH . 'auth.util.php';
 require_once UTILS_PATH . 'carts.util.php';
 
