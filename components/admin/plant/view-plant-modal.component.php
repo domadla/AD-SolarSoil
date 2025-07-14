266677d2dcd2ae985701ce591739aaaed2de4e8c
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-14 20:39:26 +0800

    refactor: change plant modal to use PostgreSQL and Admin utility class

--- components/admin/plant/view-plant-modal.component.php
+++ components/admin/plant/view-plant-modal.component.php
@@ -1,13 +1,32 @@
 <?php
 // View Plant Modal Content
-require_once __DIR__ . '/../../../handlers/plants.handler.php';
-$plants = PlantsHandler::getAllPlants();
+require_once BASE_PATH . '/bootstrap.php';
+require_once BASE_PATH . '/vendor/autoload.php';
+require_once UTILS_PATH . '/admin.util.php';
+require_once UTILS_PATH . '/auth.util.php';
+require_once UTILS_PATH . '/envSetter.util.php';
+
+Auth::init();
+
+$host = $pgConfig['host'];
+$port = $pgConfig['port'];
+$username = $pgConfig['user'];
+$password = $pgConfig['pass'];
+$dbname = $pgConfig['db'];
+
+// Connect to Postgres
+$dsn = "pgsql:host={$host};port={$port};dbname={$dbname}";
+$pdo = new PDO($dsn, $username, $password, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+]);
+
+$plants = Admin::display_plants($pdo);
 $plantRows = '';
 foreach ($plants as $plant) {
     $plantRows .= '<tr>' .
         '<td>' . htmlspecialchars($plant['id']) . '</td>' .
         '<td>' . htmlspecialchars($plant['name']) . '</td>' .
-        '<td>' . (isset($plant['stock']) ? htmlspecialchars($plant['stock']) : '-') . '</td>' .
+        '<td>' . (isset($plant['stock_quantity']) ? htmlspecialchars($plant['stock_quantity']) : '-') . '</td>' .
         '<td>' . (isset($plant['price']) ? htmlspecialchars($plant['price']) : '-') . '</td>' .
         '</tr>';
 }
