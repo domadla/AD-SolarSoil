f0b27ac270d2005f2c473b89609ec6088303abcd
MarkGamboa <202310110@fit.edu.ph>
2025-07-15 12:35:13 +0800

    fix: update edit plant modal to remove required attributes from description, price, and stock quantity fields

--- components/admin/admin-modal.component.php
+++ components/admin/admin-modal.component.php
@@ -1,4 +1,3 @@
-<!-- admin-modal.component.php -->
 <div class="modal fade" id="adminInfoModal" tabindex="-1" aria-labelledby="adminInfoModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
@@ -7,7 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body" id="adminInfoModalBody">
-                <!-- Content will be set by JS -->
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
--- components/admin/plant/edit-plant-modal.component.php
+++ components/admin/plant/edit-plant-modal.component.php
@@ -8,15 +8,15 @@
     </div>
     <div class="mb-2">
         <label class="form-label">Description</label>
-        <input type="text" class="form-control" name="description" required>
+        <input type="text" class="form-control" name="description">
     </div>
     <div class="mb-2">
         <label class="form-label">Price</label>
-        <input type="number" step="0.01" class="form-control" name="price" required>
+        <input type="number" step="0.01" class="form-control" name="price">
     </div>
     <div class="mb-2">
         <label class="form-label">Stock Quantity</label>
-        <input type="number" class="form-control" name="stock_quantity" min="0" required>
+        <input type="number" class="form-control" name="stock_quantity" min="0">
     </div>
     <div class="mb-2">
         <label class="form-label">Image URL</label>
