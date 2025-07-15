fd46813a6878d6f7f9464106f4dd6d294be2bb7d
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-16 01:35:37 +0800

    feat: add form submission functionality to plant deletion modal

--- components/admin/plant/delete-plant-modal.component.php
+++ components/admin/plant/delete-plant-modal.component.php
@@ -1,7 +1,14 @@
+<?php
+$id = $_GET['id'];
+?>
 <div class="text-center py-4">
     <p class="fs-5">Are you sure you want to delete this plant?</p>
     <div class="d-flex justify-content-center gap-3 mt-4">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-danger">Delete</button>
+        <form action="/handlers/admin.handler.php" method="POST">
+            <input type="hidden" name="action" value="delete_plant">
+            <input type="hidden" name="id" value="<?php echo htmlspecialchars($id); ?>">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
     </div>
 </div>
