e351a3e1467e50620c38ea076e53f98efc5f1872
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-16 01:35:26 +0800

    feat: add form submission functionality to delete user modal

--- components/admin/user/delete-user-modal.component.php
+++ components/admin/user/delete-user-modal.component.php
@@ -5,7 +5,11 @@ $id = $_GET['id'];
 <div class="text-center py-4">
     <p class="fs-5">Are you sure you want to delete this account?</p>
     <div class="d-flex justify-content-center gap-3 mt-4">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-danger">Delete</button>
+        <form action="/handlers/admin.handler.php" method="POST">
+            <input type="hidden" name="action" value="delete_user">
+            <input type="hidden" name="id" value="<?php echo htmlspecialchars($id); ?>">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+            <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
     </div>
 </div>
