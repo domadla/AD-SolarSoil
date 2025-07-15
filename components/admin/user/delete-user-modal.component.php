52cdb5a9e1bb90780ed33ce65df6ff02692af2e1
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-15 23:40:32 +0800

    feat: add delete user confirmation modal component for admin page

---
+++ components/admin/user/delete-user-modal.component.php
@@ -0,0 +1,23 @@
+<!-- Delete Account Modal Component -->
+<?
+$id = $_GET['id'];
+?>
+<div class="modal fade" id="deleteAccountModal" tabindex="-1" aria-labelledby="deleteAccountModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-dark text-white">
+            <div class="modal-header border-0">
+                <h5 class="modal-title" id="deleteAccountModalLabel">Delete Account</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p class="fs-5 text-center">Are you sure you want to delete this account?</p>
+            </div>
+            <div class="modal-footer border-0 justify-content-center">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
