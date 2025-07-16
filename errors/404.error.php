4893ae922c8ac221c054d63bece1d67b88e65eeb
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2025-07-16 14:35:00 +0800

    refactor: use bootstrap.php dependency and use LAYOUTS_PATH constant in 404 error page

--- errors/404.error.php
+++ errors/404.error.php
@@ -1,8 +1,9 @@
 <?php
 // 404 Error Page
+require_once 'bootstrap.php';
 http_response_code(404);
 $error_code = '404';
 $error_title = 'Page Not Found';
 $error_message = 'Sorry, the page you are looking for does not exist or may have drifted to another galaxy. You seem to have reached the edge of the SolarSoil universe.';
 
-include '../layouts/error-layout.php';
+include LAYOUTS_PATH . 'error-layout.php';
