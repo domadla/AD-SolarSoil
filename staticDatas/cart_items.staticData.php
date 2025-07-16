c285df85e8b59a0016d49461312b90ad2736d668
carlmarinyo <marinojerome2@gmail.com>
2025-07-16 11:16:11 +0800

    feat: add order_id on cart items static data

--- staticDatas/cart_items.staticData.php
+++ staticDatas/cart_items.staticData.php
@@ -3,21 +3,25 @@ return [
     [
         'cart_id' => 1,
         'plant_id' => 1,
-        'quantity' => 2
+        'quantity' => 2,
+        'order_id' => 1
     ],
     [
         'cart_id' => 1,
         'plant_id' => 2,
-        'quantity' => 1
+        'quantity' => 1,
+        
     ],
     [
         'cart_id' => 2,
         'plant_id' => 3,
-        'quantity' => 3
+        'quantity' => 3,
+        'order_id' => 2
     ],
     [
         'cart_id' => 3,
         'plant_id' => 1,
-        'quantity' => 1
+        'quantity' => 1,
+        'order_id' => 3
     ]
 ];
