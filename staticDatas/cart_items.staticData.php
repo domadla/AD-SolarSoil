a71ed51fae2c58d1017dbaaa95323b3389153530
carlmarinyo <marinojerome2@gmail.com>
2025-07-16 13:09:07 +0800

    feat: add more static data of cart items

--- staticDatas/cart_items.staticData.php
+++ staticDatas/cart_items.staticData.php
@@ -19,9 +19,19 @@ return [
         'order_id' => 2
     ],
     [
+        'cart_id' => 2,
+        'plant_id' => 2,
+        'quantity' => 3,
+    ],
+    [
         'cart_id' => 3,
         'plant_id' => 1,
         'quantity' => 1,
         'order_id' => 3
+    ],
+        [
+        'cart_id' => 3,
+        'plant_id' => 3,
+        'quantity' => 1,
     ]
 ];
