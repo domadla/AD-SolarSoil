7f92798341330a0aae2555aa78ae42aee6dca6f2
Marlo522 <202311932@fit.edu.ph>
2025-07-16 12:11:42 +0800

    feat: update profile edit modal form to include action and user ID, and change value attributes to placeholders

--- components/profile/profile-edit-modal.component.php
+++ components/profile/profile-edit-modal.component.php
@@ -8,30 +8,33 @@
                     aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form id="editProfileForm">
+
+                <form id="editProfileForm" method="POST" action="/handlers/profile-edit.handler.php">
+                    <input type="hidden" name="action" value="edit_user">
+                    <input type="hidden" id="user_id" name="user_id" value="<?php echo htmlspecialchars($user_data['user_id']); ?>">
                     <div class="mb-3">
                         <label for="editFirstName" class="form-label" style="color:#fff;"><i
                                 class="fas fa-user me-2"></i>First Name</label>
                         <input type="text" class="form-control bg-secondary text-light border-0" id="editFirstName"
-                            name="first_name" value="<?php echo htmlspecialchars($user_data['first_name']); ?>">
+                            name="first_name" placeholder="<?php echo htmlspecialchars($user_data['first_name']); ?>">
                     </div>
                     <div class="mb-3">
                         <label for="editLastName" class="form-label" style="color:#fff;"><i
                                 class="fas fa-user me-2"></i>Last Name</label>
                         <input type="text" class="form-control bg-secondary text-light border-0" id="editLastName"
-                            name="last_name" value="<?php echo htmlspecialchars($user_data['last_name']); ?>">
+                            name="last_name" placeholder="<?php echo htmlspecialchars($user_data['last_name']); ?>">
                     </div>
                     <div class="mb-3">
                         <label for="editUsername" class="form-label" style="color:#fff;"><i
                                 class="fas fa-at me-2"></i>Username</label>
                         <input type="text" class="form-control bg-secondary text-light border-0" id="editUsername"
-                            name="username" value="<?php echo htmlspecialchars($user_data['username']); ?>">
+                            name="username" placeholder="<?php echo htmlspecialchars($user_data['username']); ?>">
                     </div>
                     <div class="mb-3">
                         <label for="editAddress" class="form-label" style="color:#fff;"><i
                                 class="fas fa-map-marker-alt me-2"></i>Address</label>
                         <input type="text" class="form-control bg-secondary text-light border-0" id="editAddress"
-                            name="address" value="<?php echo htmlspecialchars($user_data['address']); ?>">
+                            name="address" placeholder="<?php echo htmlspecialchars($user_data['address']); ?>">
                     </div>
                     <div class="mb-3">
                         <label for="editJoinDate" class="form-label" style="color:#fff;"><i
@@ -40,11 +43,12 @@
                         <input type="text" class="form-control bg-secondary text-light border-0" id="editJoinDate"
                             value="<?php echo date('F j, Y', strtotime($user_data['join_date'])); ?>" disabled>
                     </div>
+                    <div class="modal-footer border-0">
+                        <button type="submit" class="btn btn-success">Save</button>
+                    </div>
                 </form>
             </div>
-            <div class="modal-footer border-0">
-                <button type="button" class="btn btn-success">Save</button>
-            </div>
+            
         </div>
     </div>
 </div>
