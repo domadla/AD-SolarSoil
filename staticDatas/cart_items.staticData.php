c0c0903c3cf8d77870c8e158c6d8f49543626ba0
carlmarinyo <marinojerome2@gmail.com>
2025-07-16 13:58:41 +0800

    refactor: remove some static datas for cart items

--- staticDatas/cart_items.staticData.php
+++ staticDatas/cart_items.staticData.php
@@ -4,34 +4,21 @@ return [
         'cart_id' => 1,
         'plant_id' => 1,
         'quantity' => 2,
+        'incart' => false,
         'order_id' => 1
-    ],
-    [
-        'cart_id' => 1,
-        'plant_id' => 2,
-        'quantity' => 1,
-        
     ],
     [
         'cart_id' => 2,
         'plant_id' => 3,
         'quantity' => 3,
+        'incart' => false,
         'order_id' => 2
-    ],
-    [
-        'cart_id' => 2,
-        'plant_id' => 2,
-        'quantity' => 3,
     ],
     [
         'cart_id' => 3,
         'plant_id' => 1,
         'quantity' => 1,
+        'incart' => false,
         'order_id' => 3
-    ],
-        [
-        'cart_id' => 3,
-        'plant_id' => 3,
-        'quantity' => 1,
     ]
 ];
